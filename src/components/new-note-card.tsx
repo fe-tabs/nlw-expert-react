a2e842da46fbd74343c759f1026877d1655a1fa2
Fernanda dos Santos <159660295+fe-tabs@users.noreply.github.com>
2024-02-11 07:29:04 -0300

    :sparkles: feat: add textarea to NewNoteCard's Dialog and toasts

--- src/components/new-note-card.tsx
+++ src/components/new-note-card.tsx
@@ -1,7 +1,30 @@
 import * as Dialog from '@radix-ui/react-dialog'
+import { ChangeEvent, FormEvent, useState } from 'react'
 import { X } from 'lucide-react'
+import { toast } from 'sonner'
 
 export function NewNoteCard() {
+  const [shouldShowOnboarding, setShouldShowOnboarding] = useState(true)
+  const [content, setContent] = useState('')
+
+  function handleStartEditor() {
+    setShouldShowOnboarding(false)
+  }
+
+  function handleContentChanged(e: ChangeEvent<HTMLTextAreaElement>) {
+    setContent(e.target.value)
+    
+    if (e.target.value == '') {
+      setShouldShowOnboarding(true)
+    }
+  }
+  
+  function handleSaveNote(e: FormEvent) {
+    e.preventDefault()
+    
+        toast.success('Nota criada com sucesso!')
+  }
+
   return (
     <Dialog.Root>
       <Dialog.Trigger className='rounded-md text-left flex flex-col bg-slate-700 p-5 gap-3 outline-none hover:ring-2 hover:ring-slate-600 focus-visible:ring-2 focus-visible:ring-lime-400'>
@@ -18,32 +41,43 @@ export function NewNoteCard() {
         <Dialog.Overlay className='inset-0 fixed bg-black/50' />
         <Dialog.Content className='fixed overflow-hidden left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 max-w-[640px] w-full h-[60vh] bg-slate-700 rounded-md flex flex-col outline-none'>
           <Dialog.Close className='absolute top-0 right-0 bg-slate-800 p-1.5 text-slate-400 hover:text-slate-100'>
-            <X className='size-5' />
+              <X className='size-5' />
           </Dialog.Close>
 
-          <div className='flex flex-col flex-1 gap-3 p-5'>
-            <span className='text-sm font-medium text-slate-300'>
-              Adicionar nota
-            </span>
-            <p className='text-sm leading-6 text-slate-400'>
-              Comece 
-                <button className='font-medium text-lime-400 hover:underline'>
-                  gravando uma nota
-                </button> 
-              em áudio ou se preferir 
-                <button className='font-medium text-lime-400 hover:underline'>
-                  utilize apenas texto
-                </button>
-              .
-            </p>
-          </div>
-
-          <button 
-            type='button'
-            className='w-full bg-lime-400 py-4 text-center text-sm text-lime-950 outline-none font-medium hover:bg-lime-500'
+          <form 
+            className='flex flex-col flex-1'
+            onSubmit={handleSaveNote}
           >
-            Salvar nota
-          </button>
+            <div className='flex flex-col flex-1 gap-3 p-5'>
+              <span className='text-sm font-medium text-slate-300'>
+                Adicionar nota
+              </span>
+              {shouldShowOnboarding ? (
+                <p className='text-sm leading-6 text-slate-400'>
+                  Comece <button 
+                    className='font-medium text-lime-400 hover:underline'
+                  > gravando uma nota </button> em áudio ou se preferir <button 
+                    className='font-medium text-lime-400 hover:underline'
+                    onClick={handleStartEditor}
+                  > utilize apenas texto</button>.
+                </p>
+              ) : (
+                <textarea 
+                  name='content'
+                  autoFocus
+                  className='text-sm leading-6 text-slate-400 bg-transparent resize-none flex-1 outline-none'
+                  onChange={handleContentChanged}
+                />
+              )}
+            </div>
+
+            <button 
+              type='submit'
+              className='w-full bg-lime-400 py-4 text-center text-sm text-lime-950 outline-none font-medium hover:bg-lime-500'
+            >
+              Salvar nota
+            </button>
+          </form>
         </Dialog.Content>
       </Dialog.Portal>
     </Dialog.Root>
